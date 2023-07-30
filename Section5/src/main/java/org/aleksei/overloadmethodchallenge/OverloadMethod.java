a47f394db93e0733c07d4caab793453a1f1063ea
Aleksei Briukhachev <abriukhachev@trask.cz>
2023-07-30 11:32:58 +0200

    finish exec

---
+++ Section5/src/main/java/org/aleksei/overloadmethodchallenge/OverloadMethod.java
@@ -0,0 +1,13 @@
+package org.aleksei.overloadmethodchallenge;
+
+public class OverloadMethod {
+    public static void main(String[] args) {
+        System.out.println(convertToCentimeters(5, 8) + " cm");
+    }
+    public static double convertToCentimeters(int inches) {
+        return inches * 2.54d;
+    }
+    public static double convertToCentimeters(int feet, int inches) {
+        return convertToCentimeters(inches + feet * 12);
+    }
+}
